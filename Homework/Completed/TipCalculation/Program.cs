f32f6f652a54e009c3a5867980a74c18c006477f
Adam Patridge <patridge@gmail.com>
2017-09-21 11:08:59 -0600

    Add files missed by migration.

---
+++ Homework/Completed/TipCalculation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TipCalculation
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Tipper tipper = new Tipper();
+
+			while (true)
+			{
+				Console.Write("Please enter check amount: ");
+				tipper.TotalBill = double.Parse(Console.ReadLine());
+
+				Console.Write("B)ad tip O)k tip G)ood Tip D)ivvy the bill Q)uit: ");
+				char operation = Console.ReadLine().ToUpper()[0];
+
+				if (operation == 'Q')
+					break;
+
+				double answer = 0.0;
+
+				switch (operation)
+				{
+					case 'B':
+						answer = tipper.CalculateTip(0.10);
+						break;
+
+					case 'O':
+						answer = tipper.CalculateTip(); // for 15% you can use the no-parameter version
+						break;
+
+					case 'G':
+						answer = tipper.CalculateTip(0.20);
+						break;
+
+					case 'D':
+						Console.Write("Number of people? ");
+						int numPeople = int.Parse(Console.ReadLine());
+						answer = tipper.Divvy(numPeople);
+						break;
+
+					default:
+						Console.WriteLine("Please enter B, O, G, D or Q");
+						break;
+				}
+
+				Console.WriteLine("Answer: {0}", answer);
+			}
+		}
+	}
+}
---
+++ Homework/Completed/TipCalculation/Tipper.cs
@@ -0,0 +1,22 @@
+﻿namespace TipCalculation
+{
+	class Tipper
+	{
+		public double TotalBill;
+
+		public double CalculateTip(double percentage)
+		{
+			return TotalBill * percentage;
+		}
+
+		public double CalculateTip()
+		{
+			return CalculateTip(0.15);
+		}
+
+		public double Divvy(int numberOfDiners)
+		{
+			return TotalBill / numberOfDiners;
+		}
+	}
+}
